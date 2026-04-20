ba4123a728b5e7f7c7330c6b242a4417602fb06d
IvanLiakh1 <liakh.ivan1@student.uzhnu.edu.ua>
2026-04-20 13:48:45 +0300

    feat: Add user Schema for DB

---
+++ Backend/models/user.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [3, "Ім'я не може бути коротшим за 3 символи"],
+        maxLength: [30, "Ім'я не може бути довшим за 20 символів"],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Довжина пароля не може бути менше 8 симовлів'],
+        select: false,
+    },
+    refreshToken: { type: String, default: null },
+});
+
+userSchema.methods.getJWTToken = function () {};
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
