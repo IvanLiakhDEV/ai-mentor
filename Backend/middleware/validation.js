0ebf45a49a167c24c7a18cac3dc60011fc7fa99f
IvanLiakh1 <liakh.ivan1@student.uzhnu.edu.ua>
2026-04-22 23:06:30 +0300

    feat: add function to valiidate request params

--- Backend/middleware/validation.js
+++ Backend/middleware/validation.js
@@ -1,7 +1,16 @@
 import { ErrorHandler } from '../utils/errorHandlers.js';
 
-export const validateSchema = schema => (req, res, next) => {
+export const validateBodySchema = schema => (req, res, next) => {
     const result = schema.safeParse(req.body ?? {});
+    if (!result.success) {
+        const message = result.error.issues[0].message;
+        return next(new ErrorHandler(message, 400));
+    }
+    req.body = result.data;
+    next();
+};
+export const validateParamsSchema = schema => (req, res, next) => {
+    const result = schema.safeParse(req.params ?? {});
     if (!result.success) {
         const message = result.error.issues[0].message;
         return next(new ErrorHandler(message, 400));
