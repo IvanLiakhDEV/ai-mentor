9c3bf649f9e99f1bada5c51472fc2d5019bb79c2
IvanLiakhDev <doomsady221312@gmail.com>
2026-04-30 16:14:52 +0300

    chore(api): add axios and react-query dependencies; setup QueryClientProvider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import { router } from './router/router';
 import './assets/style/main.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 const root = document.getElementById('root');
 
-ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(root).render(
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+    </QueryClientProvider>,
+);
