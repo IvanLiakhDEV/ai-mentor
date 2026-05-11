7298db5f5a80454af141a5d0dd65bd8eb26113e4
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-11 13:36:30 +0300

    feat(course): implement search functionality for course list

--- Frontend/src/components/CourseList/CourseList.jsx
+++ Frontend/src/components/CourseList/CourseList.jsx
@@ -1,20 +1,36 @@
+import React, { useState } from 'react';
 import { useCourses } from '@/hooks/useCourse';
 import { selectCourses, selectIsInitialized } from '@/store/selectors/courseSelectors';
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { CourseCard } from '../courseCard/CourseCard';
+import { InputField } from '../inputs/InputField';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 export const CourseList = () => {
     useCourses();
     const isInitialized = useSelector(selectIsInitialized);
     const courses = useSelector(selectCourses);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filteredCourses = courses.filter(course => {
+        const query = searchQuery.toLowerCase();
+        return course.title.toLowerCase().includes(query) || course.tags.some(tag => tag.toLowerCase().includes(query));
+    });
 
     return (
         <div className='flex flex-col gap-6'>
-            <h1 className='text-2xl font-bold'>Досліджуй каталог доступних курсів</h1>
+            <h1 className='text-2xl font-bold '>Досліджуй каталог доступних курсів</h1>
+            <div className='-mt-3.75'>
+                <InputField
+                    placeholder={'Введіть назвою або тег'}
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    icon={HiMagnifyingGlass}
+                />
+            </div>
             <div className='flex gap-10'>
                 {isInitialized ? (
-                    courses.length > 0 ? (
-                        courses.map((course, index) => (
+                    filteredCourses.length > 0 ? (
+                        filteredCourses.map((course, index) => (
                             <CourseCard
                                 courseData={course}
                                 key={index}
