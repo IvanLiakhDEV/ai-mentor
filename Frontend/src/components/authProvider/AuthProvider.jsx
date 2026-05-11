054590b1292a63072dbc220b459ee334de323e7b
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-11 10:48:07 +0300

    feat(auth): integrate useUserEnrollments hook in AuthProvider

--- Frontend/src/components/authProvider/AuthProvider.jsx
+++ Frontend/src/components/authProvider/AuthProvider.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { useMe } from '@/hooks/useAuth';
 import { useSelector } from 'react-redux';
 import { selectIsInitialized } from '@/store/selectors/authSelectors';
+import { useUserEnrollments } from '@/hooks/useEnrollment';
 export const AuthProvider = ({ children }) => {
     useMe();
+    useUserEnrollments();
     const isInitialized = useSelector(selectIsInitialized);
     if (!isInitialized) return <p>Test</p>;
 
