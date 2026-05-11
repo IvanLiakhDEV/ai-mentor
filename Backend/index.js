91e213ac4e2c0f4063e29e7e5397ef421e34f115
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-11 18:21:20 +0300

    fix(server): ensure database connection before starting the server

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,8 @@ app.use('/lesson', lessonRouter);
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
