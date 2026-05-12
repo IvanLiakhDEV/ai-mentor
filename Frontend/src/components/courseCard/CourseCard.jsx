4ff76146fae1d8f4a9fb7f8bc4c061909e7a8a4d
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-12 17:48:29 +0300

    fix(course-list): update styling and improve course card rendering

--- Frontend/src/components/courseCard/CourseCard.jsx
+++ Frontend/src/components/courseCard/CourseCard.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { Button } from '../button/Button';
 import { Tag } from '../tag/Tag';
 import { useNavigate } from 'react-router';
-export const CourseCard = ({ courseData, key }) => {
+export const CourseCard = ({ courseData }) => {
     const navigate = useNavigate();
     return (
-        <div
-            className='flex flex-col gap-5 p-4 lg:p-6 transition-colors border border-gray-200 shadow-sm cursor-pointer max-w-100 bg-bg-surface rounded-xl hover:border-blue-700 '
-            key={key}>
+        <div className='flex flex-col gap-5 p-6 transition-colors border border-gray-200 shadow-sm cursor-pointer bg-bg-surface rounded-xl hover:border-blue-700 '>
             <div className='grid gap-4'>
                 <h1 className='text-lg font-semibold'>{courseData.title}</h1>
                 <h2 className='text-secondary'>{courseData.description}</h2>
             </div>
             <div>
                 <ul className='flex gap-2'>
-                    {courseData.tags.map(tag => (
-                        <Tag title={tag} />
+                    {courseData.tags.map((tag, index) => (
+                        <Tag
+                            title={tag}
+                            key={index}
+                        />
                     ))}
                 </ul>
             </div>
