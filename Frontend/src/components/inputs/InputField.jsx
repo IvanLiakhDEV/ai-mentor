f534c3eb204b87622ead7ab58c47ef982d37e224
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-15 11:17:29 +0300

    feat(InputField): enhance input field component with textarea support

--- Frontend/src/components/inputs/InputField.jsx
+++ Frontend/src/components/inputs/InputField.jsx
@@ -1,53 +1,60 @@
+import { cn } from '@/lib/utils';
 import React, { useState } from 'react';
 import { IoMdEyeOff, IoMdEye } from 'react-icons/io';
-export const InputField = React.forwardRef(({ id, label, type = 'text', placeholder, icon: Icon, error, ...rest }, ref) => {
-    const [visible, setVisible] = useState(false);
-    const toggleVisible = () => setVisible(!visible);
-    return (
-        <div className='flex flex-col w-full gap-1'>
-            {label && (
-                <label
-                    htmlFor={id}
-                    className='font-medium'>
-                    {label}
-                </label>
-            )}
-            <div
-                className={`flex flex-row items-center bg-transparent border rounded-md p-[12px_16px_12px_40px] relative ${!Icon && 'pl-3'}`}>
-                {Icon && (
-                    <Icon
-                        className='absolute -translate-y-1/2 left-2 top-1/2'
-                        size={20}
-                        opacity={0.7}
-                    />
+export const InputField = React.forwardRef(
+    ({ id, label, type = 'text', isTextArea = false, placeholder, icon: Icon, error, className, ...rest }, ref) => {
+        const [visible, setVisible] = useState(false);
+        const toggleVisible = () => setVisible(!visible);
+        const InputComponent = isTextArea ? 'textarea' : 'input';
+        return (
+            <div className='flex flex-col w-full gap-1'>
+                {label && (
+                    <label
+                        htmlFor={id}
+                        className='font-medium'>
+                        {label}
+                    </label>
                 )}
-                <input
-                    id={id}
-                    ref={ref}
-                    type={type == 'password' ? (visible ? 'text' : 'password') : type}
-                    className='flex-1 placeholder-gray-400 bg-transparent border-none outline-none w-full'
-                    placeholder={placeholder}
-                    {...rest}
-                />
-                {type == 'password' && (
-                    <button type='button'>
-                        {visible ? (
-                            <IoMdEye
-                                size={20}
-                                opacity={0.7}
-                                onClick={toggleVisible}
-                            />
-                        ) : (
-                            <IoMdEyeOff
-                                size={20}
-                                opacity={0.7}
-                                onClick={toggleVisible}
-                            />
+                <div
+                    className={`flex flex-row items-center bg-transparent border rounded-md p-[12px_16px_12px_40px] relative ${!Icon && 'pl-3'}`}>
+                    {Icon && (
+                        <Icon
+                            className='absolute -translate-y-1/2 left-2 top-1/2'
+                            size={20}
+                            opacity={0.7}
+                        />
+                    )}
+                    <InputComponent
+                        id={id}
+                        ref={ref}
+                        {...(isTextArea ? {} : { type: type === 'password' ? (visible ? 'text' : 'password') : type })}
+                        className={cn(
+                            'flex-1 placeholder-gray-400 bg-transparent border-none outline-none w-full resize-none wrap-break-word',
+                            className,
                         )}
-                    </button>
-                )}
+                        placeholder={placeholder}
+                        {...rest}
+                    />
+                    {type == 'password' && (
+                        <button type='button'>
+                            {visible ? (
+                                <IoMdEye
+                                    size={20}
+                                    opacity={0.7}
+                                    onClick={toggleVisible}
+                                />
+                            ) : (
+                                <IoMdEyeOff
+                                    size={20}
+                                    opacity={0.7}
+                                    onClick={toggleVisible}
+                                />
+                            )}
+                        </button>
+                    )}
+                </div>
+                {error && <p className='-mb-3 text-sm font-medium text-red-500'>{error}</p>}
             </div>
-            {error && <p className='-mb-3 text-sm font-medium text-red-500'>{error}</p>}
-        </div>
-    );
-});
+        );
+    },
+);
