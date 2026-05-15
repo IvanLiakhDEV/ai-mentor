bce493d4d45d9a16bfa4c1d3e4c465839cabaafc
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-15 12:36:51 +0300

    feat(course): add course validation schema and create course API function

--- Frontend/src/api/course.api.js
+++ Frontend/src/api/course.api.js
@@ -8,3 +8,7 @@ export const fetchCourseById = async id => {
     const response = await api.get(`/course/${id}`);
     return response.data;
 };
+export const createCourse = async data => {
+    const response = await api.post(`/course`, data);
+    return response.data;
+};
