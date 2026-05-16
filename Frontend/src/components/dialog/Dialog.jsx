88ad325b65c4e93ea4588ed51643254e618002ee
IvanLiakhDev <doomsady221312@gmail.com>
2026-05-16 16:39:49 +0300

    feat(dialog): enhance dialog layout with improved styling and overflow handling

--- Frontend/src/components/dialog/Dialog.jsx
+++ Frontend/src/components/dialog/Dialog.jsx
@@ -8,8 +8,10 @@ export const Dialog = ({ children, visible = false, headerTitle, onClose, sumbit
         <form
             className={cn('fixed inset-0 bg-black/50 hidden items-center justify-center p-4 z-50', visible && 'flex')}
             onSubmit={onSubmit}>
-            <div className='bg-white rounded-xl shadow-xl max-w-2xl w-full'>
-                <header className='flex justify-between border-b p-6 items-center w-full'>
+            <div
+                className='bg-white rounded-xl shadow-xl max-w-2xl w-full max-h-3/4 
+             '>
+                <header className='flex justify-between border-b p-4 px-6 items-center w-full flex-1'>
                     <h2 className='text-xl font-bold text-gray-900'>{headerTitle}</h2>
                     <span
                         onClick={onClose}
@@ -17,8 +19,8 @@ export const Dialog = ({ children, visible = false, headerTitle, onClose, sumbit
                         <IoIosClose className='w-6 h-6' />
                     </span>
                 </header>
-                <main className='py-4 px-6 gap-4 flex flex-col'>{children}</main>
-                <footer className='flex justify-between gap-6 px-6 py-3'>
+                <main className='py-4 px-6 gap-4 flex flex-col overflow-y-auto h-96'>{children}</main>
+                <footer className='flex justify-between gap-6 px-6 py-3 flex-1'>
                     <Button
                         variant='secondary'
                         size='lg'
