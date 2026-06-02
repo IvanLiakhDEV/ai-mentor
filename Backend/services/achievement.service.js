1ccf40f91ac841ca5c172ffa763131b3a5ff5873
IvanLiakhDev <doomsady221312@gmail.com>
2026-06-02 16:27:04 +0300

    feat(achievement, streak): break executeCode service into reusable functions

--- Backend/services/achievement.service.js
+++ Backend/services/achievement.service.js
@@ -16,6 +16,35 @@ export const getUserStats = async userId => {
 export const getAllAchievements = async () => {
     return await Achievement.find({});
 };
-export const updateStreak = async () => {
-    return;
+
+export const checkAndUnlockAchievements = async ({ userId, updatedStats }) => {
+    const newlyUnlocked = [];
+    const achievementConditions = [
+        { conditionType: 'lessons_completed', targetValue: { $lte: updatedStats.lessonsCompleted } },
+        { conditionType: 'courses_completed', targetValue: { $lte: updatedStats.coursesCompleted } },
+        { conditionType: 'points_earned', targetValue: { $lte: updatedStats.points } },
+        { conditionType: 'day_streak', targetValue: { $lte: updatedStats.currentStreak } },
+    ];
+    if (new Date().getHours() < 4) {
+        achievementConditions.push({
+            conditionType: 'night_lesson_completed',
+            targetValue: { $lte: 1 },
+        });
+    }
+    const availableAchievements = await Achievement.find({ $or: achievementConditions });
+    for (const achievement of availableAchievements) {
+        const { upsertedCount } = await UserAchievement.updateOne(
+            { userId, achievementId: achievement._id },
+            { $setOnInsert: { userId, achievementId: achievement._id } },
+            { upsert: true },
+        );
+        if (upsertedCount) {
+            newlyUnlocked.push(achievement);
+            if (achievement.xpReward > 0) {
+                await UserStat.findOneAndUpdate({ userId }, { $inc: { points: achievement.xpReward } });
+            }
+        }
+    }
+
+    return newlyUnlocked;
 };
